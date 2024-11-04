c4b5b2d9a0aef10df57acb0b0076c70defe1403c
Andres <hoyos@purdue.edu>
2024-11-03 23:20:20 -0500

    Added 3 publishers for each lidar

--- rosbag_ws/src/bag_reading_cpp/src/bag_spoofed_reader.cpp
+++ rosbag_ws/src/bag_reading_cpp/src/bag_spoofed_reader.cpp
@@ -17,9 +17,14 @@ public:
     PlaybackSpoofedNode(const std::string & bag_filename, bool loop)
     : Node("play_bag_spoofed_node"), loop_(loop), bag_filename_(bag_filename), message_count_(0)
     {
-        publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/perception/luminar_front/points", 10);
+
+        publisher_front_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/perception/luminar_front/points", 10);
+        publisher_left_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/perception/luminar_left/points", 10);
+        publisher_right_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/perception/luminar_right/points", 10);
+        
         timer_ = this->create_wall_timer(
-                35ms, std::bind(&PlaybackSpoofedNode::timer_callback, this));
+            35ms, std::bind(&PlaybackSpoofedNode::timer_callback, this));
+
 
         rosbag2_storage::StorageOptions storage_options;
         storage_options.uri = bag_filename_;
@@ -40,7 +45,7 @@ private:
                 auto ros_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
                 serialization_.deserialize_message(&serialized_msg, ros_msg.get());
 
-                publisher_->publish(*ros_msg);
+                publisher_front_points_->publish(*ros_msg);
                 message_count_++;
                 RCLCPP_DEBUG(this->get_logger(), "Published message");
 
@@ -49,6 +54,35 @@ private:
                 double frequency = message_count_ / elapsed_time.seconds();
                 RCLCPP_DEBUG(this->get_logger(), "Publish frequency: %.2f Hz", frequency);
             }
+            if (msg->topic_name == "/perception/luminar_left/points") {
+                rclcpp::SerializedMessage serialized_msg(*msg->serialized_data);
+                auto ros_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
+                serialization_.deserialize_message(&serialized_msg, ros_msg.get());
+
+                publisher_left_points_->publish(*ros_msg);
+                message_count_++;
+                RCLCPP_DEBUG(this->get_logger(), "Published message");
+
+                auto current_time = this->now();
+                auto elapsed_time = current_time - start_time_;
+                double frequency = message_count_ / elapsed_time.seconds();
+                RCLCPP_DEBUG(this->get_logger(), "Publish frequency: %.2f Hz", frequency);
+            }
+            if (msg->topic_name == "/perception/luminar_right/points") {
+                rclcpp::SerializedMessage serialized_msg(*msg->serialized_data);
+                auto ros_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
+                serialization_.deserialize_message(&serialized_msg, ros_msg.get());
+
+                publisher_right_points_->publish(*ros_msg);
+                message_count_++;
+                RCLCPP_DEBUG(this->get_logger(), "Published message");
+
+                auto current_time = this->now();
+                auto elapsed_time = current_time - start_time_;
+                double frequency = message_count_ / elapsed_time.seconds();
+                RCLCPP_DEBUG(this->get_logger(), "Publish frequency: %.2f Hz", frequency);
+            }
+
         } else if (loop_) {
             // Close and reopen the reader to loop playback
             reader_->close();
@@ -60,7 +94,9 @@ private:
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_front_points_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_left_points_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_right_points_;
     rclcpp::Serialization<sensor_msgs::msg::PointCloud2> serialization_;
     std::unique_ptr<rosbag2_cpp::Reader> reader_;
     bool loop_;
